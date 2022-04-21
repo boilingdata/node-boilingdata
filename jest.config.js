57914e2eb16ef87ce7c2ef0871dcbe1192215b24
Dan Forsberg <dforsber@gmail.com>
2022-04-21 21:02:46 +0300

    initial base files

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {
+        "^.+\\.ts$": "ts-jest",
+    },
+    testRegex: "(/src/.*(\\.)(test|spec))\\.(jsx?|tsx?)$",
+    moduleFileExtensions: ["js", "ts"],
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/{!(index),}.ts"],
+    coverageReporters: ["json-summary", "text", "lcov"],
+};
+  
