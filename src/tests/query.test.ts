a4857b63810c7ac946fa80ca0d483a2996f17913
Dan Forsberg <dforsber@gmail.com>
2022-08-12 22:18:43 +0300

    Add support for JS hooks query parameter

--- src/tests/query.test.ts
+++ src/tests/query.test.ts
@@ -1,5 +1,5 @@
 import { EEngineTypes, globalCallbacksList, IBDDataResponse } from "../boilingdata/boilingdata.api";
-import { BDAWSRegion, BoilingData, isDataResponse } from "../boilingdata/boilingdata";
+import { BDAWSRegion, BoilingData, IJsHooks, isDataResponse } from "../boilingdata/boilingdata";
 import { createLogger } from "bunyan";
 
 jest.setTimeout(90000);
@@ -508,6 +508,93 @@ describe("BoilingData with S3 folders", () => {
           "count_star()": 29166808,
         },
       ]
+    `);
+  });
+});
+
+describe("BoilingData JS query hooks", () => {
+  beforeAll(async () => {
+    bdInstance = new BoilingData({ username, password, globalCallbacks, logLevel });
+    await bdInstance.connect();
+    logger.info("connected.");
+  });
+
+  afterAll(async () => {
+    await bdInstance.close();
+    logger.info("connection closed.");
+  });
+
+  it("transforms query results to (naive) CSV", async () => {
+    const jsHooks: IJsHooks = {
+      initFunc: (sql: string, scanCursor: number) => "",
+      headerFunc: (c: any, first: any) => [c, Object.keys(first).join(",")],
+      batchFunc: (c: any, rows: any[]) => [c, rows.map(r => Object.values(r).join(","))],
+      footerFunc: (c: any, total: number) => `total rows: ${total}`,
+      finalFunc: (c: any, allRows: any[]) => allRows, // identity func
+    };
+    const rows = await new Promise<any[]>((resolve, reject) => {
+      const r: any[] = [];
+      bdInstance.execQuery({
+        sql: `SELECT * FROM parquet_scan('s3://eu-north-1-boilingdata-demo/test.parquet:m=0') LIMIT 5;`,
+        jsHooks: jsHooks,
+        keys: [],
+        callbacks: {
+          onData: (data: IBDDataResponse | unknown) => {
+            if (isDataResponse(data)) data.data.map(row => r.push(row));
+            resolve(r);
+          },
+          onLogError: (data: any) => reject(data),
+        },
+      });
+    });
+    expect(rows.join("\n")).toMatchInlineSnapshot(`
+      "registration_dttm,id,first_name,last_name,email,gender,ip_address,cc,country,birthdate,salary,title,comments
+      1454486129000,1,Amanda,Jordan,ajordan0@com.com,Female,1.197.201.2,6759521864920116,Indonesia,3/8/1971,49756.53,Internal Auditor,1E+02
+      1454519043000,2,Albert,Freeman,afreeman1@is.gd,Male,218.111.175.34,,Canada,1/16/1968,150280.17,Accountant IV,
+      1454461771000,3,Evelyn,Morgan,emorgan2@altervista.org,Female,7.161.136.94,6767119071901597,Russia,2/1/1960,144972.51,Structural Engineer,
+      1454459781000,4,Denise,Riley,driley3@gmpg.org,Female,140.35.109.83,3576031598965625,China,4/8/1997,90263.05,Senior Cost Accountant,
+      1454475931000,5,Carlos,Burns,cburns4@miitbeian.gov.cn,,169.113.235.40,5602256255204850,South Africa,,,,
+      total rows: 5"
+    `);
+  });
+
+  it("transforms query results to simple HTML Table", async () => {
+    const jsHooks: IJsHooks = {
+      initFunc: (sql: string, scanCursor: number) => "",
+      headerFunc: (c: any, first: any) => [
+        c,
+        "<table><tr><th>".concat(Object.keys(first).join("</th><th>")).concat("</th></tr>"),
+      ],
+      batchFunc: (c: any, rows: any[]) => [
+        c,
+        rows.map(r => "<tr><td>".concat(Object.values(r).join("</td><td>")).concat("</td></tr>")),
+      ],
+      footerFunc: (c: any, total: number) => `</table><br><b>Total rows: ${total}<br>`,
+      finalFunc: (c: any, allRows: any[]) => allRows, // identity func
+    };
+    const rows = await new Promise<any[]>((resolve, reject) => {
+      const r: any[] = [];
+      bdInstance.execQuery({
+        sql: `SELECT * FROM parquet_scan('s3://eu-north-1-boilingdata-demo/test.parquet:m=0') LIMIT 5;`,
+        jsHooks: jsHooks,
+        keys: [],
+        callbacks: {
+          onData: (data: IBDDataResponse | unknown) => {
+            if (isDataResponse(data)) data.data.map(row => r.push(row));
+            resolve(r);
+          },
+          onLogError: (data: any) => reject(data),
+        },
+      });
+    });
+    expect(rows.join("\n")).toMatchInlineSnapshot(`
+      "<table><tr><th>registration_dttm</th><th>id</th><th>first_name</th><th>last_name</th><th>email</th><th>gender</th><th>ip_address</th><th>cc</th><th>country</th><th>birthdate</th><th>salary</th><th>title</th><th>comments</th></tr>
+      <tr><td>1454486129000</td><td>1</td><td>Amanda</td><td>Jordan</td><td>ajordan0@com.com</td><td>Female</td><td>1.197.201.2</td><td>6759521864920116</td><td>Indonesia</td><td>3/8/1971</td><td>49756.53</td><td>Internal Auditor</td><td>1E+02</td></tr>
+      <tr><td>1454519043000</td><td>2</td><td>Albert</td><td>Freeman</td><td>afreeman1@is.gd</td><td>Male</td><td>218.111.175.34</td><td></td><td>Canada</td><td>1/16/1968</td><td>150280.17</td><td>Accountant IV</td><td></td></tr>
+      <tr><td>1454461771000</td><td>3</td><td>Evelyn</td><td>Morgan</td><td>emorgan2@altervista.org</td><td>Female</td><td>7.161.136.94</td><td>6767119071901597</td><td>Russia</td><td>2/1/1960</td><td>144972.51</td><td>Structural Engineer</td><td></td></tr>
+      <tr><td>1454459781000</td><td>4</td><td>Denise</td><td>Riley</td><td>driley3@gmpg.org</td><td>Female</td><td>140.35.109.83</td><td>3576031598965625</td><td>China</td><td>4/8/1997</td><td>90263.05</td><td>Senior Cost Accountant</td><td></td></tr>
+      <tr><td>1454475931000</td><td>5</td><td>Carlos</td><td>Burns</td><td>cburns4@miitbeian.gov.cn</td><td></td><td>169.113.235.40</td><td>5602256255204850</td><td>South Africa</td><td></td><td></td><td></td><td></td></tr>
+      </table><br><b>Total rows: 5<br>"
     `);
   });
 });
